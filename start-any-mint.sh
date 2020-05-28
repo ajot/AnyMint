9c170846cae41a22d1cd06915c90675cb1a4eaf6
Amit Jotwani <hello@ajot.me>
2020-05-28 08:58:08 -0400

    Updating the method to open AnyBar

--- start-any-mint.sh
+++ start-any-mint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-`open ~/Applications/AnyBar.app`
+open -a AnyBar
 
 # TODO: Update the path to point to AnyMint/main.rb
-`ruby PATH_TO/AnyMint/main.rb`
+ruby PATH_TO/AnyMint/main.rb
